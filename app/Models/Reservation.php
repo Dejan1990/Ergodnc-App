9d1513325f22fcfc5604f30d55e9fdbb1ba32ed5
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-09-28 17:48:44 +0200

    Prepare resources

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -9,6 +9,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Reservation extends Model
 {
     use HasFactory;
+    
+    const STATUS_ACTIVE = 1;
+    const STATUS_CANCELLED = 2;
 
     protected $casts = [
         'price' => 'integer',
--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -2,9 +2,10 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\User;
 use App\Models\Office;
+use App\Models\Reservation;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
@@ -22,7 +23,7 @@ class ReservationFactory extends Factory
             'user_id' => User::factory(),
             'office_id' => Office::factory(),
             'price' => $this->faker->numberBetween(10_000, 20_000),
-            'status' => 1,
+            'status' => Reservation::STATUS_ACTIVE,
             'start_date' => now()->addDay(1)->format('Y-m-d'),
             'end_date' => now()->addDay(5)->format('Y-m-d'),
         ];
