c94525cb91d1a39d598d478fab87c36682fb893e
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-09-28 17:21:14 +0200

    Prepare factories

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Office;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reservation>
@@ -17,7 +19,12 @@ class ReservationFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'office_id' => Office::factory(),
+            'price' => $this->faker->numberBetween(10_000, 20_000),
+            'status' => 1,
+            'start_date' => now()->addDay(1)->format('Y-m-d'),
+            'end_date' => now()->addDay(5)->format('Y-m-d'),
         ];
     }
 }
--- database/migrations/2022_09_28_134620_create_images_table.php
+++ database/migrations/2022_09_28_134620_create_images_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('office_id')->index();
-            $table->string('image');
+            $table->morphs('resource');
+            $table->string('path');
             $table->timestamps();
         });
     }
