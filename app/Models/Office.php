0c2c2d1bdf3d1233645f034eca4108f40fa72aeb
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-09-29 17:54:20 +0200

    Show only approved and visible records, Filters by hosts and users, Paginate

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
@@ -39,5 +40,10 @@ class Office extends Model
     public function images(): MorphMany
     {
         return $this->morphMany(Image::class, 'resource');
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'offices_tags');
     }
 }
--- tests/Feature/OfficeControllerTest.php
+++ tests/Feature/OfficeControllerTest.php
@@ -2,17 +2,20 @@
 
 namespace Tests\Feature;
 
-use App\Models\Office;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Tag;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Office;
+use App\Models\Reservation;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class OfficeControllerTest extends TestCase
 {
     use RefreshDatabase;
 
     /** @test */
-    public function itListsOffices()
+    public function itListsAllOfficesInPaginatedWay()
     {
         Office::factory(3)->create();
 
@@ -23,6 +26,99 @@ class OfficeControllerTest extends TestCase
         $response->assertOk();
         $response->assertJsonCount(3, 'data');
         //$this->assertCount(3, $response->json('data'));
+
         $this->assertNotNull($response->json('data')[0]['id']);
+        $this->assertNotNull($response->json('meta'));
+        $this->assertNotNull($response->json('links'));
+    }
+
+    /**
+     * @test
+     */
+    public function itOnlyListsOfficesThatAreNotHiddenAndApproved()
+    {
+        Office::factory(3)->create();
+
+        Office::factory()->create(['approval_status' => Office::APPROVAL_PENDING]);
+        Office::factory()->create(['hidden' => true]);
+
+        $response = $this->get('/api/offices');
+        //dd($response->json('data'));
+        $response->assertOk();
+        $response->assertJsonCount(3, 'data');
+    }
+
+    /**
+     * @test
+     */
+    public function itFiltersByHostId()
+    {
+        Office::factory(3)->create();
+
+        $host = User::factory()->create();
+        $office = Office::factory()->for($host)->create();
+
+        $response = $this->get('/api/offices?host_id=' . $host->id);
+
+        $response->assertJsonCount(1, 'data');
+        $this->assertEquals($office->id, $response->json('data')[0]['id']);
+    }
+
+    /**
+     * @test
+     */
+    public function itFiltersByUserId()
+    {
+        Office::factory(3)->create();
+
+        $user = User::factory()->create();
+        $office = Office::factory()->for($user)->create();
+
+        Reservation::factory()->create();
+        Reservation::factory()->for($office)->for($user)->create();
+
+        $response = $this->get('/api/offices?user_id=' . $user->id);
+        $response->assertJsonCount(1, 'data');
+        $this->assertEquals($office->id, $response->json('data')[0]['id']);
+    }
+
+    /**
+     * @test
+     */
+    public function itIncludesImagesTagsAndUser()
+    {
+        $user = User::factory()->create();
+        $tag = Tag::factory()->create();
+        $office = Office::factory()->for($user)->create();
+
+        $office->tags()->attach($tag);
+        $office->images()->create(['path' => 'image.jpg']);
+
+        $response = $this->get('/api/offices');
+        //dd($response->json('data'));
+
+        $response->assertOk();
+        $this->assertIsArray($response->json('data')[0]['tags']);
+        $this->assertCount(1, $response->json('data')[0]['tags']);
+        $this->assertIsArray($response->json('data')[0]['images']);
+        $this->assertCount(1, $response->json('data')[0]['images']);
+        //$this->assertEquals($user->id, $response->json('data')[0]['user']['id']);
+        $this->assertEquals($user->id, $response->json('data')[0]['user_id']);
+    }
+
+    /**
+     * @test
+     */
+    public function itReturnsTheNumberOfActiveReservations()
+    {
+        $office = Office::factory()->create();
+
+        Reservation::factory(7)->for($office)->create(['status' => Reservation::STATUS_ACTIVE]);
+        Reservation::factory()->for($office)->create(['status' => Reservation::STATUS_CANCELLED]);
+
+        $response = $this->get('/api/offices');
+        //dd($response->json('data'));
+        $response->assertOk();
+        $this->assertEquals(7, $response->json('data')[0]['reservations_count']);
     }
 }
