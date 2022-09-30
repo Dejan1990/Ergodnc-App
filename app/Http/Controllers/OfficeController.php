1ea4cf37251571bb9c57d744d898a768be50c41d
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-09-30 15:33:49 +0200

    Sort by distance if lng/lat is provided

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -26,6 +26,10 @@ class OfficeController extends Controller
                 fn (Builder $builder) => 
                     $builder->whereRelation('reservations', 'user_id', '=', request('user_id'))
             )
+            ->when(request('lat') && request('lng'),
+                fn ($builder) => $builder->nearestTo(request('lat'), request('lng')),
+                fn ($builder) => $builder->orderBy('id', 'ASC')
+            )
             ->latest('id')
             ->with(['images', 'tags', 'user'])
             ->withCount(['reservations' => fn ($builder) => $builder->where('status', Reservation::STATUS_ACTIVE)])
